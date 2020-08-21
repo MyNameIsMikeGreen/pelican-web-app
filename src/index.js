713ac71508b092c4e237b4b8b8416cf597081601
MyNameIsMikeGreen <mikegreen1995@outlook.com>
2020-08-21 15:29:07 +0100

    Poll server for status

--- src/index.js
+++ src/index.js
@@ -3,9 +3,23 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Main extends React.Component {
+
+    status = setInterval(() =>  {fetch('http://192.168.5.101:8000/status')
+        .then(response => response.json())
+        .then(data => this.setState({ status: data.status }))
+        .catch((error) => console.error(error));}, 1000);
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            status: "NOT CONNECTED",
+        };
+    }
+
     render() {
         return (
             <>
+                <h1>Status: {this.state.status}</h1>
                 <ActionButton label={"ACTIVATE"} action={() => actionActivate()}/>
                 <ActionButton label={"DEACTIVATE"} action={() => actionDeactivate()}/>
             </>
